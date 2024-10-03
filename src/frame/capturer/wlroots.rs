d1285055c97193ecf0e78af63c3e75168ae8f675
Rishik Yalamanchili <73787402+Rishik-Y@users.noreply.github.com>
2024-10-03 22:22:28 +0530

    Removed comments

--- src/frame/capturer/wlroots.rs
+++ src/frame/capturer/wlroots.rs
@@ -41,7 +41,6 @@ impl super::Capturer for Capturer {
 
         let unwraped_list = Rc::try_unwrap(self.globallists).unwrap();
 
-        // Obtain the queue handle from your event queue
         let queue_handle: &QueueHandle<Capturer> = &self.event_queue.borrow().handle();
 
         cloned_list
@@ -49,10 +48,10 @@ impl super::Capturer for Capturer {
             .filter(|Global| Global.interface == "wl_output")
             .for_each(|Global| {
                 let output = Rc::new(self.registry.bind::<WlOutput, GlobalListContents, Capturer>(
-                    Global.name,  // name
-                    1,            // version
-                    queue_handle, // queue handle
-                    unwraped_list,    // user data
+                    Global.name, 
+                    1,           
+                    queue_handle, 
+                    unwraped_list,  
                 ));
                 let capturer = Rc::new(self.clone());
                 let controller = controller.clone();
@@ -90,16 +89,12 @@ impl Default for Capturer {
         let mut event_queue = display.new_event_queue();
         let attached_display = display.display();
 
-        // Create the QueueHandle
         let queue_handle: QueueHandle<Capturer> = event_queue.handle();
 
         let globals = GlobalList::new(&attached_display);
-        let globallists = globals.clone_list(); // Store the GlobalListContents
-
-        // Update the get_registry call
+        let globallists = globals.clone_list(); 
         let registry = attached_display.get_registry(&queue_handle, &globallists);
 
-        // Instead of passing &mut self, we can use a local mutable variable
         let mut capturer_instance = Self {
             event_queue: Rc::new(RefCell::new(event_queue)),
             globallists,
@@ -114,13 +109,12 @@ impl Default for Capturer {
             vulkan: Rc::new(Vulkan::new().expect("Unable to initialize Vulkan")),
         };
 
-        // Now you can roundtrip using the mutable reference
         capturer_instance.event_queue
             .borrow_mut()
             .roundtrip(&mut capturer_instance)
             .unwrap();
 
-        capturer_instance // Return the instance
+        capturer_instance 
     }
 }
 
@@ -128,16 +122,14 @@ impl Capturer {
     fn capture_frame(self: Rc<Self>, controller: Rc<RefCell<Controller>>, output: Rc<WlOutput>) {
         let mut frame = Object::default();
 
-        // Obtain the queue handle from your event queue
         let queue_handle: &QueueHandle<Capturer> = &self.event_queue.borrow().handle();
 
-        // Capture the frame using the dmabuf_manager
         self.dmabuf_manager
             .capture_output(
-                0,                            // overlay_cursor
-                &output,                      // output
-                queue_handle,                 // queue handle
-                Rc::try_unwrap(self.globallists).unwrap() // dereference Rc to get GlobalListContents
+                0,                          
+                &output,                      
+                queue_handle,                
+                Rc::try_unwrap(self.globallists).unwrap() 
             );
             .quick_assign(move |data, event, _| match event {
                 Event::Frame {
@@ -150,7 +142,7 @@ impl Capturer {
                 }
 
                 Event::Object { index, fd, size, .. } => {
-    let fd_raw = fd.into_raw_fd(); // Convert OwnedFd to i32
+    let fd_raw = fd.into_raw_fd(); 
                     frame.set_object(index, fd_raw, size);
                 }
 
@@ -185,13 +177,12 @@ impl Capturer {
     }
 }
 
-// Implement the Dispatch trait for handling WlOutput events
 impl Dispatch<WlOutput, GlobalListContents> for Capturer {
     fn event(
         _state: &mut Self,
         _proxy: &WlOutput,
         event: <WlOutput as wayland_client::Proxy>::Event,
-        _udata: &GlobalListContents, // Replaced MyUserData with GlobalListContents
+        _udata: &GlobalListContents, 
         _conn: &wayland_client::Connection,
         _qh: &QueueHandle<Self>,
     ) {
@@ -238,7 +229,7 @@ impl Dispatch<ZwlrExportDmabufFrameV1, GlobalListContents> for Capturer {
         _state: &mut Self,
         _proxy: &ZwlrExportDmabufFrameV1,
         event: wayland_protocols_wlr::export_dmabuf::v1::client::zwlr_export_dmabuf_frame_v1::Event,
-        _udata: &GlobalListContents, // Replaced MyUserData with GlobalListContents
+        _udata: &GlobalListContents, 
         _conn: &wayland_client::Connection,
         _qh: &QueueHandle<Self>,
     ) {
@@ -272,7 +263,7 @@ impl Dispatch<ZxdgOutputV1, GlobalListContents> for Capturer {
         _state: &mut Self,
         _proxy: &ZxdgOutputV1,
         event: <ZxdgOutputV1 as wayland_client::Proxy>::Event,
-        _udata: &GlobalListContents, // Now using GlobalListContents
+        _udata: &GlobalListContents,
         _conn: &wayland_client::Connection,
         _qh: &QueueHandle<Self>,
     ) {
@@ -305,12 +296,11 @@ impl Dispatch<WlRegistry, GlobalListContents> for Capturer {
         _state: &mut Self,
         _proxy: &WlRegistry,
         event: <WlRegistry as wayland_client::Proxy>::Event,
-        _globals: &GlobalListContents, // Now we pass GlobalListContents instead of custom user data
+        _globals: &GlobalListContents, 
         _conn: &wayland_client::Connection,
         _qh: &QueueHandle<Self>,
     ) {
         match event {
-            // Handle when a new global object is announced
             wayland_client::protocol::wl_registry::Event::Global {
                 name,
                 interface,
@@ -323,15 +313,11 @@ impl Dispatch<WlRegistry, GlobalListContents> for Capturer {
                     name
                 );
 
-                // Here you can bind specific globals when announced
                 if interface == "wl_output" {
-                    // You can bind the global here if needed
                     log::info!("Binding wl_output global (name: {})", name);
-                    // Example: self.bind_output(name, version); // Actual binding would happen here
                 }
             }
 
-            // Handle when a global object is removed
             wayland_client::protocol::wl_registry::Event::GlobalRemove { name } => {
                 log::info!("Global removed: name={}", name);
             }
